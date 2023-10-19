f05ac439746b756cc598b575c463b9eddad4e321
Avishkar05 <144703543+Avishkar05@users.noreply.github.com>
2023-10-19 21:32:51 +0530

    write a program for to create a deadlock scenario programmatically in Java

---
+++ Day-20.java
@@ -0,0 +1,44 @@
+//write a program for to create a deadlock scenario programmatically in Java
+public class DeadlockDemo {
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("Thread 1: Holding lock 1...");
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+
+                System.out.println("Thread 1: Waiting for lock 2...");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: Acquired lock 2!");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("Thread 2: Holding lock 2...");
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+
+                System.out.println("Thread 2: Waiting for lock 1...");
+                synchronized (lock1) {
+                    System.out.println("Thread 2: Acquired lock 1!");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        // Uncomment the following lines to prevent the program from terminating
+      /*
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        */
+    }
+}
