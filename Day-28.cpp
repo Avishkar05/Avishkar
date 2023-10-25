a47873b20b64d1d0802d238f646071d0f23af958
Avishkar05 <144703543+Avishkar05@users.noreply.github.com>
2023-10-25 19:09:23 +0530

    Write a program to print ' Hello world' 1000 Times without using loop

---
+++ Day-28.cpp
@@ -0,0 +1,19 @@
+// Write a program to print ' Hello world' 1000 Times without using loop
+#include <iostream>
+using namespace std;
+
+void printHelloWorld(int n) {
+    if (n <= 0) {
+        return;
+    }
+
+    cout << "Hello  world" << endl;
+    printHelloWorld(n - 1);
+}
+
+int main() {
+    int times = 1000;
+    printHelloWorld(times);
+
+    return 0;
+}
